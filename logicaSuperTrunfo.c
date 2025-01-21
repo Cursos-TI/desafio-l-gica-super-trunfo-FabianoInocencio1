807e3678ec6957eb47ea367d876b093cea673a0d
FabianoInocencio1 <fa.inocencio28@gmail.com>
2025-01-21 16:49:17 +0000

    Adicionado a lógica do jogo (if else)

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,38 +6,85 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    printf("Desafio Super Trunfo - Países\n");
+    printf("\n**************************\n");
 
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    int codigoDaCarta1, codigoDaCarta2, populacao1, populacao2;
+    char nome1[50], nome2[50];
+    float area1, area2, pib1, pib2, DensidadePopulacional1, DensidadePopulacional2, PibPercapita1, PibPercapita2;
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    // Entrada para a primeira carta
+    printf("Adicione o código da carta 1: \n");
+    scanf("%d", &codigoDaCarta1);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    printf("Adicione o nome da cidade 1: \n");
+    scanf("%s", nome1);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    printf("Digite a área territorial (em KM²) da cidade 1: \n");
+    scanf("%f", &area1);
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    printf("Digite o PIB da cidade 1: \n");
+    scanf("%f", &pib1);
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    printf("Digite a população da cidade 1: \n");
+    scanf("%d", &populacao1);
 
+    // Entrada para a segunda carta
+
+     printf("\n*******************\n");
+
+    printf("Adicione o código da carta 2: \n");
+    scanf("%d", &codigoDaCarta2);
+
+    printf("Adicione o nome da cidade 2: \n");
+    scanf("%s", nome2);
+
+    printf("Digite a área territorial (em KM²) da cidade 2: \n");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB da cidade 2: \n");
+    scanf("%f", &pib2);
+
+    printf("Digite a população da cidade 2: \n");
+    scanf("%d", &populacao2);
+
+    // Cálculos
+    DensidadePopulacional1 = populacao1 / area1;
+    PibPercapita1 = pib1 / populacao1;
+
+    DensidadePopulacional2 = populacao2 / area2;
+    PibPercapita2 = pib2 / populacao2; 
+
+
+    // Exibindo as informações da primeira carta
+    printf("\n----- Informações da primeira carta -----\n");
+    printf("Código da carta 1: %d\n", codigoDaCarta1);
+    printf("Cidade: %s\n", nome1);
+    printf("Área Territorial: %.2f KM²\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("População: %d\n", populacao1);
+    printf("Densidade Populacional: %.2f habitantes por KM²\n", DensidadePopulacional1);
+    printf("PIB Per Capita: %.2f\n", PibPercapita1);
+
+    // Exibindo as informações da segunda carta
+    printf("\n----- Informações da segunda carta -----\n");
+    printf("Código da carta 2: %d\n", codigoDaCarta2);
+    printf("Cidade: %s\n", nome2);
+    printf("Área Territorial: %.2f KM²\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("População: %d\n", populacao2);
+    printf("Densidade Populacional: %.2f habitantes por KM²\n", DensidadePopulacional2);
+    printf("PIB Per Capita: %.2f\n", PibPercapita2);
+
+   
+    //Lógica do jogo 
+
+    printf ("\n**** RESULTADO*****\n");
+
+   if (DensidadePopulacional1 > DensidadePopulacional2 && PibPercapita1 < PibPercapita2) {
+     printf("%s Ganhou\n", nome1);
+} else {
+    printf("%s Ganhou\n", nome2);
+}
     return 0;
 }
